2be9c69ce2720e593471aa35694c71d471a186d7
Web-romans <roma88gruzd88@mail.ru>
2021-12-17 15:52:20 +0300

    hero, advantage, card, adaptive refactoring

--- #src/js/myscript.js
+++ #src/js/myscript.js
@@ -1,18 +1,64 @@
+var header = document.querySelector('.header');
+var headerBurger = document.querySelector('.burger');
+var headerMenu = document.querySelector('.nav');
+var search = document.querySelector('.search-form');
+var userbox = document.querySelector('.userbox');
+//var overlay = document.getElementById('nav');
+//var btns = document.getElementById('btns');
 
-var navItem = document.querySelectorAll('.nav__item');
+headerBurger.addEventListener('click', function () {
+    header.classList.toggle("mobile")
+    this.classList.toggle("close");
+    headerMenu.classList.toggle("open");
+    search.classList.toggle("mobile");
+    userbox.classList.toggle("mobile")
+    //overlay.classList.toggle("overlay");
+    //btns.classList.toggle("mobile");
+});
 
-for (var i = 0; i < navItem.length; i++) {
-    navItem[i].addEventListener('click', showSubMenu);
-    console.log(navItem[i]);
-}
+/* Меню фикс при ресайзе */
+window.addEventListener('resize', function (event) {
+    if (window.innerWidth >= 980) {
+        header.classList.remove("mobile")
+        headerBurger.classList.remove("close");
+        headerMenu.classList.remove("open");
+        search.classList.remove("mobile");
+        userbox.classList.remove("mobile")
+    }
+}, true);
 
-function showSubMenu() {
-    var subMenu = this.querySelector('sub-menu');
-    if (subMenu) {
-        if (subMenu.classList.contains('sub-menu--show')) {
-            subMenu.classList.remove('sub-menu--show');
-        } else {
-            subMenu.classList.add('sub-menu--show');
+
+var heroSwiper = new Swiper(".hero-swiper", {
+
+    centeredSlides: true,
+    spaceBetween: 32,
+    loop: true,
+    loopedSlides: 5,
+    slideClass: "hero-swiper__slide",
+    navigation: {
+        nextEl: ".hero-swiper__button--next",
+        prevEl: ".hero-swiper__button--prev",
+    },
+    pagination: {
+        el: ".hero-swiper__pagination",
+        clickable: true,
+    },
+    /*
+    breakpoint: {
+        320: {
+            slidesPerView: "auto",
+            centeredSlides: true,
+            navigation: true,
+            pagination: true,
+        },
+        560: {
+            slidesPerView: 2,
+            centeredSlides: true,
+        },
+        980: {
+            slidesPerView: "auto",
+            centeredSlides: true,
         }
     }
-}
+    */
+});
